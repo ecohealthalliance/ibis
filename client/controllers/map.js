33f11de68c40bf69ba1f4bbce79197a66a28716a
Brock Arnold <arnold@ecohealthalliance.org>
2017-10-26 12:15:23 -0400

    Map view

---
+++ client/controllers/map.js
@@ -0,0 +1,17 @@
+Template.map.onCreated(function () {
+
+});
+ 
+Template.map.onRendered(function () {
+  L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
+  var map = L.map('map');
+  L.tileLayer.provider('Stamen.Watercolor').addTo(map);
+  map.setView([40.077946, -95.989253], 4);
+
+});
+ 
+
+Template.map.helpers({
+   
+});
+

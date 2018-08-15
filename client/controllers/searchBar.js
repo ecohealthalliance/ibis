61de601c86b9db8209d466276a903351ee01d6f0
Nathan Breit <nath@nathanathan.com>
2018-08-15 19:30:09 +0000

    handle parens in query, add search result icons

--- client/controllers/searchBar.js
+++ client/controllers/searchBar.js
@@ -1,28 +1,34 @@
 import locationGeoJsonPromise from '/imports/locationGeoJsonPromise';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+const regexEscape = (s)=>s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+
 Template.searchBar.onCreated(function() {
   this.searchResults = new ReactiveVar()
   this.typeaheadPromise = Promise.all([
     locationGeoJsonPromise, 
-    new Promise((resolve, reject) =>{
-      HTTP.get('/api/bioeventNames', (err, resp)=> {
+    new Promise((resolve, reject)=>{
+      HTTP.get('/api/bioeventNames', (err, resp)=>{
         if(err) return reject(err);
         resolve(resp.data);
       });
     })
   ]).then(([locationGeoJson, bioeventNames])=>{
     return _.map(locationGeoJson, (x, id)=>{
+      const [locationType, locationName] = id.split(':');
+      const type = {};
+      type[locationType] = true;
       return {
-        id: "locaitons/" + id,
-        name: x.displayName
+        type: type,
+        id: "locations/" + id,
+        name: x.displayName + ` (${locationName})`
       };
-    }).concat(bioeventNames);
+    }).concat(bioeventNames.map((name)=>{
+      name.type = {bioevent: true};
+      return name;
+    }));
   });
-  $(document).on('click', ()=> this.searchResults.set(null));
-});
-
-Template.searchBar.onRendered(function() {
+  $(document).on('click', ()=>this.searchResults.set(null));
 });
 
 Template.searchBar.helpers({
@@ -30,13 +36,12 @@ Template.searchBar.helpers({
 });
 
 Template.searchBar.events({
-  'keypress #search': _.debounce((event, instance)=>{
+  'keyup #search': _.debounce((event, instance)=>{
     const $input = instance.$('#search');
     if($input.val().length == 0) return
-    const searchRE = new RegExp($input.val().split(/[^a-z]/i).join("."), "i");
+    const searchRE = new RegExp(regexEscape($input.val()), "i");
     instance.typeaheadPromise.then((items)=>{
       instance.searchResults.set(items.filter(x=>searchRE.test(x.name)).slice(0, 10))
     });
   })
 });
-  
